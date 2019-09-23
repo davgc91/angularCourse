a94c8404d6a196d6b97e2d19f711e5627080c87b
David Gonzalez Carlos <dgoncarl@usersad.everis.int>
2019-09-23 11:49:45 -0500

    procesamiento por data

--- formularios/src/app/components/data/data.component.ts
+++ formularios/src/app/components/data/data.component.ts
@@ -10,13 +10,26 @@ export class DataComponent {
 
   forma:FormGroup;
 
+  usuario:object = {
+    nombrecompleto: { 
+      nombre:"David",
+      apellido:"Gonzalez"
+    },
+    correo:"david@gmail.com"
+
+  };
+
 
   constructor() { 
 
-    this.forma = new FormGroup({  
-      'nombre': new FormControl('',   [Validators.required,
-                                       Validators.minLength(3)]),
-      'apellido': new FormControl('', Validators.required),
+    console.log(this.usuario);
+
+    this.forma = new FormGroup({
+      'nombrecompleto' : new FormGroup({
+          'nombre': new FormControl('',   [Validators.required,
+                                           Validators.minLength(3)]),
+          'apellido': new FormControl('', Validators.required)
+      }),      
       'correo': new FormControl('',   [Validators.required,
                                        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
     });
